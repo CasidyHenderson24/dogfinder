066b1a0c380240964b96cb6a4e74c620f6654143
CasidyHenderson24 <casidyhenderson24@gmail.com>
2014-05-20 14:37:19 -0400

    Added Herokufiles

--- app/views.py
+++ app/views.py
@@ -2,8 +2,25 @@
 
 from app import app
 from flask import render_template 
-@app.route('/')
+
+@app. route('/')
+
+@app.route('/index')
 def index():
 	return render_template('petdraft.html')
 
+@app.route('/first')
+def first():
+	return render_template('first.html')
+
+
+
+@app.route('/second')
+def second():
+	return render_template('second.html')
+
+
+@app.route('/third')
+def third():
+	return render_template('third.html')
 
---
+++ runp-heroku.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#!flask/bin/python 
+
+#launches the app
+
+from app import app 
